308f6caa1d58bb3c5e6993852f605a792627593e
Abidkhan8004 <mohammadabid1978004@gmail.com>
2024-10-31 17:21:04 +0500

    ListGroup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ListGroup from "./componenets/ListGroup";
+
+function App() {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+
+function Message() {
+  // JSX: Javascript XML
+
+  const name = "Abid";
+  if (name) return <h1>Code here {name}</h1>;
+  return <h1>Code here</h1>;
+}
+
+export default Message;
