61909ccdab6afb063da68dc2fdb883d95438d863
Abidkhan8004 <mohammadabid1978004@gmail.com>
2024-11-15 01:10:04 +0500

    Handling Events

--- src/componenets/ListGroup.tsx
+++ src/componenets/ListGroup.tsx
@@ -1,15 +1,27 @@
-function ListGroup() {
-  let items = ["New York", "Delhi", "Karachi", "Paris", "London"];
+import { MouseEvent } from "react";
 
-  items = [];
+function ListGroup() {
+  let items = [
+    "New York",
+    "Delhi",
+    "Karachi",
+    "Paris",
+    "London",
+    "Dera Ismail Khan",
+  ];
+
+  // Event Handler
+  const handleclick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>Items Not Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleclick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
